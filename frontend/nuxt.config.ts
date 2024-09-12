e70bb91f10d13e09e27b0a4a4ce50b932fd85ab0
SugarMilk23 <h_tan@matisse.co.jp>
2024-09-12 22:10:58 +0900

    nuxt.config.ts edited

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,10 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
-  ssr: true, // Ensure SSR is enabled
-  // Remove this line
-  // target: "server",
-  // Modify the router configuration
+  ssr: false, // Change to false for static site generation
+  nitro: {
+    preset: "static",
+  },
   app: {
     head: {
       link: [
@@ -21,19 +21,14 @@ export default defineNuxtConfig({
       ],
     },
   },
-  // Update router configuration
-  router: {
-    options: {
-      // Use this instead of fallback
-      strict: false,
-    },
-  },
   compatibilityDate: "2024-04-03",
   modules: ["@pinia/nuxt"],
   devtools: { enabled: true },
 
   runtimeConfig: {
-    apiKey: process.env.API_KEY,
-    token: process.env.API_READ_ACCESS_TOKEN,
+    public: {
+      apiKey: process.env.NUXT_PUBLIC_API_KEY,
+      token: process.env.NUXT_PUBLIC_API_READ_ACCESS_TOKEN,
+    },
   },
 });
